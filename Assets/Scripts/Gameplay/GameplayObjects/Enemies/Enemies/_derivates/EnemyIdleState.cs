006eb14a1fbc24fa89bae58d39197ffb1c45bba1
tolosa5 <diegorinconesteban@gmail.com>
2023-12-14 23:11:17 +0100

    enemies integration ++

--- Assets/Scripts/Gameplay/GameplayObjects/Enemies/Enemies/_derivates/EnemyIdleState.cs
+++ Assets/Scripts/Gameplay/GameplayObjects/Enemies/Enemies/_derivates/EnemyIdleState.cs
@@ -67,7 +67,6 @@ public class EnemyIdleState : EnemyState
                     stunnedTimerOn = false;
                 }
             }
-            enemyDetection.OnPlayerDetection();
             patrolAI.PatrolLogic();
         }
         return this;
