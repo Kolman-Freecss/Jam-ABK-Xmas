303e94133ab01862716bbf8754a8a9da66b639e9
NunezCotanoRuben <151525856+NunezCotanoRuben@users.noreply.github.com>
2023-12-15 02:29:48 +0100

    Puzzle Fix
    Fixed some puzzle issues. Now the ElectricityPuzzle and SequencePuzzle are playable.
    
    I take out of the playable puzzles the LeverPuzzle because it did not work propertly.

--- Assets/Scripts/Gameplay/GameplayObjects/RoundComponents/Puzzles/PuzzleTypes/PuzzleInstantiable/Window/LeverPuzzle.cs
+++ Assets/Scripts/Gameplay/GameplayObjects/RoundComponents/Puzzles/PuzzleTypes/PuzzleInstantiable/Window/LeverPuzzle.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Puzzle;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class LeverPuzzle : PuzzleController
 {
@@ -10,40 +11,51 @@ public class LeverPuzzle : PuzzleController
     [SerializeField]
     List<bool> correctLeverPositions = new List<bool>();
     List<bool> playerLeverPositions = new List<bool>();
+    Toggle[] checks;
 
     void Awake()
     {
+        checks = GetComponentsInChildren<Toggle>();
         // Initialize player switch positions
         for (int i = 0; i < correctLeverPositions.Count; i++)
         {
+            Debug.Log(i);
             playerLeverPositions.Add(false);
         }
+
+        base.TotalSteps = 0;
+        for (int i = 0; i < correctLeverPositions.Count; i++)
+        {
+            if (correctLeverPositions[i])
+            {
+                base.TotalSteps++;
+            }
+        }
+        base.StepsToFail = TotalSteps * 2;
     }
 
     public LeverPuzzle() : base()
     {
-        base.StepsToFail = correctLeverPositions.Count;
-        base.TotalSteps = correctLeverPositions.Count;
-
-        // Initialize player switch positions
-        for (int i = 0; i < correctLeverPositions.Count; i++)
-        {
-            playerLeverPositions.Add(false);
-        }
+        
     }
 
     public void ToggleSwitch(int switchIndex)
     {
+        Debug.Log("Player before actualization" + playerLeverPositions.Count);
         // Toggle the switch position
         playerLeverPositions[switchIndex] = !playerLeverPositions[switchIndex];
         if (switchIndex + 1 < playerLeverPositions.Count)
         {
+            Debug.Log("checks" + checks.Length);
+            Debug.Log("player" + playerLeverPositions.Count);
             playerLeverPositions[switchIndex + 1] = !playerLeverPositions[switchIndex + 1];
+            checks[switchIndex + 1].isOn = playerLeverPositions[switchIndex + 1];
         }
 
         if (switchIndex - 1 > 0)
         {
             playerLeverPositions[switchIndex - 1] = !playerLeverPositions[switchIndex - 1];
+            checks[switchIndex - 1].isOn = playerLeverPositions[switchIndex - 1];
         }
 
         // Check if the puzzle is solved
--- Assets/Scripts/Gameplay/GameplayObjects/RoundComponents/Puzzles/PuzzleTypes/PuzzleInstantiable/Window/SequencePuzzle.cs
+++ Assets/Scripts/Gameplay/GameplayObjects/RoundComponents/Puzzles/PuzzleTypes/PuzzleInstantiable/Window/SequencePuzzle.cs
@@ -1,64 +1,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using Puzzle;
+using TMPro;
+using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SequencePuzzle : PuzzleController
 {
     [Header("General Variables")]
     [SerializeField]
-    int totalSteps;
+    int stepsToFail;
 
     [SerializeField]
-    int stepsToFail;
+    int totalSteps;
 
     [Header("Puzzle Variables")]
     [SerializeField]
     int firstNumber;
 
+    [SerializeField]
+    TextMeshProUGUI text;
+    TMP_InputField text1;
+
     List<int> correctSequence = new List<int>();
     List<int> playerSequence = new List<int>();
 
-    void Start()
+    private void Awake()
     {
         StepsToFail = stepsToFail;
-        TotalSteps = totalSteps;
+        TotalSteps = 1;
 
         PuzzleProgress(true);
         firstNumber = Random.Range(1, 15);
 
-        // Generate the Fibonacci sequence
-        if (firstNumber == 0 || firstNumber == 1)
-            correctSequence.Add(1);
         correctSequence.Add(firstNumber == 0 ? 1 : firstNumber);
-        correctSequence = Enumerable.Range(1, totalSteps).Select(x => Fibonacci(x)).ToList();
-        Debug.Log(correctSequence.Count);
+        correctSequence = Enumerable.Range(1, StepsToFail).Select(x => Fibonacci(x, firstNumber)).ToList();
+        text.text = "| ";
+        for (int i = 0; i < correctSequence.Count; i++)
+        {
+            if (i != correctSequence.Count - 1)
+            {
+                text.text += correctSequence[i] + " | ";
+            }
+        }
     }
 
-    void Update() { }
+
+    public int Fibonacci(int n, int firstNumber)
+    {
+        if (n <= 1)
+            return firstNumber;
+        else
+        {
+            int fib = 0;
+            int a = firstNumber;
+            int b = firstNumber;
+            for (int i = 2; i <= n; i++)
+            {
+                fib = a + b;
+                a = b;
+                b = fib;
+            }
+            return fib;
+        }
+    }
+
+    public void EnterNumber()
+    {
+        text1 = GetComponentInChildren<TMP_InputField>();
+        try
+        {
+            playerSequence.Add(int.Parse(text1.text));
+            foreach (int i in correctSequence)
+            {
+                Debug.Log(i);
+            }
+            Debug.Log(
+                "Player sequence" + playerSequence[0] + "Correct sequence" + correctSequence[correctSequence.Count - 1]
+            );
+            UpdateProgress(playerSequence[playerSequence.Count - 1] == correctSequence[correctSequence.Count - 1]);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
 
     public override void OnPuzzleInteract()
     {
-        EnterNumber(1);
-        if (puzzleState.state == PuzzleStates.SOLVED)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private static int Fibonacci(int n)
-    {
-        return n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
-    }
-
-    public void EnterNumber(int number)
-    {
-        playerSequence.Add(number);
-        foreach (int i in correctSequence)
-        {
-            Debug.Log(i);
-        }
-        PuzzleProgress(playerSequence[playerSequence.Count - 1] == correctSequence[playerSequence.Count - 1]);
+        throw new System.NotImplementedException();
     }
 }
